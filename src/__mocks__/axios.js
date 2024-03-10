a69aa7e015d77b30d81aef2b1e284e743750069d
Andrea Pani <andre@Andreas-MacBook-Pro.local>
2024-03-10 17:59:12 +0100

    fix: axios mock

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,28 +1,13 @@
 module.exports = {
-	get: jest.fn((url) => {
-		if (url === '/something') {
-			return Promise.resolve({
-				data: {
-					data: {
-						results: ['hero1', 'hero2', 'hero3'],
-					},
-				},
-			});
-		}
-	}),
-	post: jest.fn((url) => {
-		if (url === '/something') {
-			return Promise.resolve({
-				data: 'data',
-			});
-		}
-		if (url === '/something2') {
-			return Promise.resolve({
-				data: 'data2',
-			});
-		}
-	}),
-	create: jest.fn(function () {
-		return this;
-	}),
-};
+  get: jest.fn((url) => {
+    if (url === '/something') {
+      return Promise.resolve({
+        data: {
+          data: {
+            results: ['hero1', 'hero2', 'hero3'],
+          },
+        },
+      })
+    }
+  }),
+}
